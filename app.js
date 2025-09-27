6b837aa387779c2e08b0d67a1ae321c495a429c5
Arindam Sengupta <arindamsengupta93@gmail.com>
2025-09-28 01:24:51 +0530

    Added files

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
-const express=require('express');
-const app=express();
-const path=require('path');
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const ownersRouter=require('./routes/ownersRouter');
+const productsRouter=require('./routes/productRouter');
+const usersRouter=require('./routes/usersRouter');
+
+const db = require("./config/mongoose-connection");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.set("view engine", "ejs");
+
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
+
+app.listen(3000);
--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,23 +1,12 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://127.0.0.1:27017/ecommerce", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => {
-    console.log("Connected to MongoDB successfully!");
-})
-.catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-});
+mongoose
+  .connect("mongodb://127.0.0.1:27017/scatch")
+  .then(function(){
+      console.log("connected");
+  })
+  .catch(function(err){
+      console.log(err);
+  })
 
-// Optional: additional connection error handling
-const db = mongoose.connection;
-
-db.on("error", (err) => {
-    console.error("MongoDB connection error:", err);
-});
-
-db.once("open", () => {
-    console.log("MongoDB connection is open.");
-});
+module.exports = mongoose.connection;
